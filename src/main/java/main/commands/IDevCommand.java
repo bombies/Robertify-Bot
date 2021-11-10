c0884d28e4efdaa8ef19fc30ed9d67b312eaca31
bombies <xjamesdzn@gmail.com>
2021-11-09 23:40:02 -0500

    Refactor IDevCommand#permissionCheck

--- src/main/java/main/commands/IDevCommand.java
+++ src/main/java/main/commands/IDevCommand.java
@@ -1,17 +1,11 @@
 package main.commands;
 
+import lombok.SneakyThrows;
 import main.utils.database.BotUtils;
 
 public interface IDevCommand extends ICommand {
+    @SneakyThrows
     default boolean permissionCheck(CommandContext ctx) {
-        try {
-            if (!new BotUtils().isDeveloper(ctx.getMessage().getAuthor().getId()))
-                return false;
-
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
+        return new BotUtils().isDeveloper(ctx.getMessage().getAuthor().getId());
     }
 }
