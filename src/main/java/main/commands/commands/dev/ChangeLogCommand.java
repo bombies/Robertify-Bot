f455f4c1d08546d1080658a13d12e54c40dc4ea6
bombies <xjamesdzn@gmail.com>
2021-12-28 18:25:47 -0500

    Update changelog logo

--- src/main/java/main/commands/commands/dev/ChangeLogCommand.java
+++ src/main/java/main/commands/commands/dev/ChangeLogCommand.java
@@ -64,7 +64,7 @@ public class ChangeLogCommand implements IDevCommand {
 
         logs.forEach(log -> logsToString.append("**—** ").append(log).append("\n\n"));
         EmbedBuilder eb = EmbedUtils.embedMessage(logsToString.toString());
-        eb.setThumbnail(BotConstants.ROBERTIFY_CHRISTMAS_LOGO_TRANSPARENT.toString());
+        eb.setThumbnail(BotConstants.ROBERTIFY_LOGO_TRANSPARENT.toString());
         eb.setFooter("Note: You can toggle changelogs for this server off by doing \"toggle changelogs\"");
 
         eb.setTitle("["+GeneralUtils.formatDate(new Date().getTime(), TimeFormat.MM_DD_YYYY)+"]");
