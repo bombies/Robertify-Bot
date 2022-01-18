aa655440a0fc109df713b50dc0bbfcceef3ee9cf
bombies <xjamesdzn@gmail.com>
2022-01-18 11:15:24 -0500

    Update theme images

--- src/main/java/main/constants/RobertifyTheme.java
+++ src/main/java/main/constants/RobertifyTheme.java
@@ -5,15 +5,15 @@ import net.dv8tion.jda.api.entities.Emoji;
 import java.awt.*;
 
 public enum RobertifyTheme {
-    GREEN("https://i.imgur.com/Ct3A6Jv.png", "https://i.imgur.com/5TOxGsy.png", "https://i.imgur.com/nbuENrG.png", Emoji.fromMarkdown("<:robertify:929627982656598116>"), Color.decode("#2ce629")),
-    RED("https://i.imgur.com/TRtUFtp.png", "https://i.imgur.com/Nz84NqQ.png", "https://i.imgur.com/kfBTk5Q.png", Emoji.fromMarkdown("<:robertifyred:929638640001376318>"), Color.decode("#e62929")),
-    GOLD("https://i.imgur.com/2VTsHjX.png", "https://i.imgur.com/zTsYhFt.png", "https://i.imgur.com/uVHIMWF.png", Emoji.fromMarkdown("<:robertifygold:929627983298306049>"), Color.decode("#ffac38")),
-    PINK("https://i.imgur.com/8yK0By9.png", "https://i.imgur.com/AHjvBhq.png", "https://i.imgur.com/LhWZj9d.png", Emoji.fromMarkdown("<:robertifypink:929638640391421972>"), Color.decode("#f159ff")),
-    PURPLE("https://i.imgur.com/5MHnrDj.png", "https://i.imgur.com/5CknPoJ.png", "https://i.imgur.com/izE0wEw.png", Emoji.fromMarkdown("<:robertifypurple:929627984233644072>"), Color.decode("#8900de")),
-    BLUE("https://i.imgur.com/4uj58ui.png", "https://i.imgur.com/PJ5VsN2.png", "https://i.imgur.com/RLUmQZW.png", Emoji.fromMarkdown("<:robertifyblue:929638640043315202>"), Color.decode("#2b59ff")),
-    LIGHT_BLUE("https://i.imgur.com/8sHRtyz.png", "https://i.imgur.com/p7UwYE1.png", "https://i.imgur.com/CguiS5y.png", Emoji.fromMarkdown("<:robertifylightblue:929638640370470982>"), Color.decode("#2bd8ff")),
-    ORANGE("https://i.imgur.com/DypLd48.png", "https://i.imgur.com/vHwEUMn.png", "https://i.imgur.com/7rpX8cT.png", Emoji.fromMarkdown("<:robertifyorange:929638639942631457>"), Color.decode("#f25500")),
-    YELLOW("https://i.imgur.com/j98p7b2.png", "https://i.imgur.com/2JpiLgP.png", "https://i.imgur.com/ohAgso0.png", Emoji.fromMarkdown("<:robertifyyellow:929638640437575720>"), Color.decode("#ffea2b"));
+    GREEN("https://i.imgur.com/ZFsTohQ.png", "https://i.imgur.com/oqVpzpV.png", "https://i.imgur.com/7vZpP2B.png", Emoji.fromMarkdown("<:robertifygreen2:933027476555309056>"), Color.decode("#2ce629")),
+    RED("https://i.imgur.com/kPEgTan.png", "https://i.imgur.com/krfEYJP.png", "https://i.imgur.com/XRRcWJr.png", Emoji.fromMarkdown("<:robertifyred2:933027476660170783>"), Color.decode("#e62929")),
+    GOLD("https://i.imgur.com/W6DLRII.png", "https://i.imgur.com/tPfRDTN.png", "https://i.imgur.com/wdOp9qS.png", Emoji.fromMarkdown("<:robertifygold2:933027476739854366>"), Color.decode("#ffac38")),
+    PINK("https://i.imgur.com/sWia6Qj.png", "https://i.imgur.com/jirfaxT.png", "https://i.imgur.com/hkr4gDW.png", Emoji.fromMarkdown("<:robertifypink2:933027476639211570>"), Color.decode("#f159ff")),
+    PURPLE("https://i.imgur.com/lHsH4qL.png", "https://i.imgur.com/XZ0dsEg.png", "https://i.imgur.com/09Vgk7E.png", Emoji.fromMarkdown("<:robertifypurple2:933027476534341722>"), Color.decode("#8900de")),
+    BLUE("https://i.imgur.com/PkFNv8I.png", "https://i.imgur.com/EMPuJOU.png", "https://i.imgur.com/stJW754.png", Emoji.fromMarkdown("<:robertifyblue2:933027476752441374>"), Color.decode("#2b59ff")),
+    LIGHT_BLUE("https://i.imgur.com/aS3RAfF.png", "https://i.imgur.com/IAN6WSf.png", "https://i.imgur.com/gsCGnY4.png", Emoji.fromMarkdown("<:robertifylightblue2:933027476748259328>"), Color.decode("#2bd8ff")),
+    ORANGE("https://i.imgur.com/huYU6mh.png", "https://i.imgur.com/klJLrBn.png", "https://i.imgur.com/rWt38F3.png", Emoji.fromMarkdown("<:robertifyorange2:933027476672765982>"), Color.decode("#f25500")),
+    YELLOW("https://i.imgur.com/VSaar3x.png", "https://i.imgur.com/rGYC1TZ.png", "https://i.imgur.com/ZJVB0XJ.png", Emoji.fromMarkdown("<:robertifyyellow2:933027476723093514>"), Color.decode("#ffea2b"));
 
     private final String transparent;
     private final String idleChannel;
