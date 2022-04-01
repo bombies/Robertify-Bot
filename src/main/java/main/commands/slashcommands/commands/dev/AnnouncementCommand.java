414d2e36081af7d2b361d128cd6437ef0a5e69d7
bombies <xjamesdzn@gmail.com>
2022-03-31 22:36:02 -0500

    Change from the usage of ShardManager#getGuilds to ShardManager#getGuildCache

--- src/main/java/main/commands/slashcommands/commands/dev/AnnouncementCommand.java
+++ src/main/java/main/commands/slashcommands/commands/dev/AnnouncementCommand.java
@@ -65,7 +65,7 @@ public class AnnouncementCommand extends AbstractSlashCommand implements IDevCom
                                             Button.of(ButtonStyle.DANGER, "devannouncement:no", "No").asDisabled()
                                     )
                             ).queue(buttonsChanged -> {
-                                for (var guild : Robertify.shardManager.getGuilds()) {
+                                for (var guild : Robertify.shardManager.getGuildCache()) {
                                     if (!new TogglesConfig().getToggle(guild, Toggles.GLOBAL_ANNOUNCEMENTS))
                                         continue;
 
