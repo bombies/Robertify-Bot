4df5ae66de5ca0bebb47ee6773018d74f661b8f6
bombies <xjamesdzn@gmail.com>
2023-03-19 08:34:50 -0500

    Update Spring API exception handler

--- src/main/java/api/routes/ApiExceptionHandler.java
+++ src/main/java/api/routes/ApiExceptionHandler.java
@@ -1,8 +1,19 @@
 package api.routes;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> internalErrorHandler(Exception ex, WebRequest req) {
+        return handleExceptionInternal(ex, "Internal Server Error", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, req);
+    }
 }
