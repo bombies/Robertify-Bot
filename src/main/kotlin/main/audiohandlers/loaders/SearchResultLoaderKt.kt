0f85cb750c7aedb40a3c3a115b43f814d84edaa4
Ajani Green <xjamesdzn@gmail.com>
2023-04-19 09:58:30 -0500

    Complete SearcResultLoader Kotlin implementation

---
+++ src/main/kotlin/main/audiohandlers/loaders/SearchResultLoaderKt.kt
@@ -0,0 +1,84 @@
+package main.audiohandlers.loaders
+
+import com.github.topisenpai.lavasrc.spotify.SpotifySourceManager
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import main.utils.GeneralUtilsKt
+import main.utils.RobertifyEmbedUtilsKt
+import main.utils.component.interactions.selectionmenu.StringSelectMenuOptionKt
+import main.utils.component.interactions.selectionmenu.StringSelectionMenuBuilderKt
+import main.utils.json.themes.ThemesConfigKt
+import main.utils.locale.LocaleManagerKt
+import main.utils.locale.messages.RobertifyLocaleMessageKt
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.entities.emoji.Emoji
+import net.dv8tion.jda.api.interactions.InteractionHook
+
+class SearchResultLoaderKt(
+    private val guild: Guild,
+    private val searcher: User,
+    private val query: String,
+    private val botMessage: InteractionHook
+) : AudioLoadResultHandler {
+    override fun trackLoaded(track: AudioTrack?) {
+        throw UnsupportedOperationException("This operation is not supported in the search result loader")
+    }
+
+    override fun playlistLoaded(playlist: AudioPlaylist?) {
+        if (playlist == null)
+            return
+
+        if (!playlist.isSearchResult)
+            throw UnsupportedOperationException("This operation is not supported in the search result loader")
+
+        val tracks          = playlist.tracks
+        val localeManager   = LocaleManagerKt.getLocaleManager(guild)
+        val embedDesc       = StringBuilder()
+
+        val options = tracks.subList(0, tracks.size.coerceAtMost(10)).mapIndexed { i, track ->
+            val info        = track.info
+            val label       = "${info.title} by ${info.author}"
+            val safeLabel   = label.substring(0, label.length.coerceAtMost(100))
+
+            embedDesc.append("**${i + 1}.** - ${info.title} by ${info.author} [${GeneralUtilsKt.formatTime(info.length)}]\n")
+
+            StringSelectMenuOptionKt(
+                label = safeLabel,
+                value = "https://open.spotify.com/track/${info.identifier}",
+                emoji = Emoji.fromUnicode(GeneralUtilsKt.parseNumEmoji(i + 1))
+            )
+        }
+
+        val selectionMenu = StringSelectionMenuBuilderKt(
+            _name = "",
+            placeholder = localeManager.getMessage(RobertifyLocaleMessageKt.SearchMessages.SEARCH_MENU_PLACEHOLDER),
+            range = Pair(1,1),
+            _options = options
+        ).build()
+
+        botMessage.sendMessageEmbeds(
+            RobertifyEmbedUtilsKt.embedMessage(guild, embedDesc.toString())
+                .setAuthor(
+                    localeManager.getMessage(
+                        RobertifyLocaleMessageKt.SearchMessages.SEARCH_EMBED_AUTHOR,
+                        Pair("{query}", query.replaceFirst(SpotifySourceManager.SEARCH_PREFIX, ""))
+                    ),
+                    null,
+                     ThemesConfigKt(guild).theme.transparent
+                )
+                .setFooter(localeManager.getMessage(RobertifyLocaleMessageKt.SearchMessages.SEARCH_EMBED_FOOTER))
+                .build()
+        ).queue()
+    }
+
+    override fun noMatches() {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadFailed(exception: FriendlyException?) {
+        TODO("Not yet implemented")
+    }
+}
