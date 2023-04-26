289cbf56c8d7b0fa52194a71ed96bfac6e9a7ee8
bombies <xjamesdzn@gmail.com>
2023-04-26 18:13:38 -0500

    Add ResumeCommand

--- src/main/kotlin/main/commands/slashcommands/SlashCommandManagerKt.kt
+++ src/main/kotlin/main/commands/slashcommands/SlashCommandManagerKt.kt
@@ -71,7 +71,8 @@ object SlashCommandManagerKt {
             MoveCommandKt(),
             PauseCommandKt(),
             PreviousTrackCommandKt(),
-            RemoveCommandKt()
+            RemoveCommandKt(),
+            ResumeCommandKt()
         )
         addManagementCommands()
         addMiscCommands()
---
+++ src/main/kotlin/main/commands/slashcommands/audio/ResumeCommandKt.kt
@@ -0,0 +1,50 @@
+package main.commands.slashcommands.audio
+
+import main.audiohandlers.RobertifyAudioManagerKt
+import main.utils.RobertifyEmbedUtilsKt
+import main.utils.RobertifyEmbedUtilsKt.Companion.replyWithEmbed
+import main.utils.component.interactions.slashcommand.AbstractSlashCommandKt
+import main.utils.component.interactions.slashcommand.models.CommandKt
+import main.utils.json.logs.LogTypeKt
+import main.utils.json.logs.LogUtilsKt
+import main.utils.locale.messages.RobertifyLocaleMessageKt
+import net.dv8tion.jda.api.entities.GuildVoiceState
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+class ResumeCommandKt : AbstractSlashCommandKt(CommandKt(
+    name = "resume",
+    description = "Resume the currently paused track."
+)) {
+
+    override suspend fun handle(event: SlashCommandInteractionEvent) {
+        val memberVoiceState = event.member!!.voiceState!!
+        val selfVoiceState = event.guild!!.selfMember.voiceState!!
+        event.replyWithEmbed { handleResume(memberVoiceState, selfVoiceState) }.queue()
+    }
+
+    private fun handleResume(memberVoiceState: GuildVoiceState, selfVoiceState: GuildVoiceState): MessageEmbed {
+        val acChecks = audioChannelChecks(memberVoiceState, selfVoiceState, songMustBePlaying = true)
+        if (acChecks != null) return acChecks
+        val guild = selfVoiceState.guild
+        val player = RobertifyAudioManagerKt.getMusicManager(guild)
+            .player
+
+        if (!player.isPaused)
+            return RobertifyEmbedUtilsKt.embedMessage(guild, RobertifyLocaleMessageKt.PauseMessages.PLAYER_NOT_PAUSED).build()
+
+        player.isPaused = false
+        LogUtilsKt(guild).sendLog(
+            LogTypeKt.PLAYER_RESUME,
+            RobertifyLocaleMessageKt.PauseMessages.RESUMED_LOG,
+            Pair("{user}", memberVoiceState.member.asMention)
+        )
+        return RobertifyEmbedUtilsKt.embedMessage(guild, RobertifyLocaleMessageKt.PauseMessages.RESUMED).build()
+    }
+
+    override val help: String
+        get() = """
+                Resumes the currently playing song if paused
+
+                Usage: `/resume`"""
+}
