7a5d1b6a0ea984bab08b2ebb38cb001e722c2a27
bombies <xjamesdzn@gmail.com>
2023-05-12 11:49:52 -0500

    Remove Java files

--- src/main/java/main/utils/component/AbstractInteraction.java
+++
@@ -1,8 +0,0 @@
-package main.utils.component;
-
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-public abstract class AbstractInteraction extends ListenerAdapter {
-
-
-}
--- src/main/java/main/utils/pagination/pages/MessagePage.java
+++
@@ -1,8 +0,0 @@
-package main.utils.pagination.pages;
-
-import net.dv8tion.jda.api.entities.MessageEmbed;
-
-public abstract class MessagePage {
-    public abstract MessageEmbed getEmbed();
-
-}
