5ea64bf66bc735ab26f63bcd7a02f72ecf57ed85
bombies <xjamesdzn@gmail.com>
2023-08-15 16:47:29 -0400

    Potential fix for failing CI builds

--- src/main/kotlin/api/plugins/Koin.kt
+++ src/main/kotlin/api/plugins/Koin.kt
@@ -3,11 +3,16 @@ package api.plugins
 import api.utils.prodModule
 import api.utils.testModule
 import io.ktor.server.application.*
+import org.koin.core.error.KoinAppAlreadyStartedException
 import org.koin.ktor.plugin.Koin
 
 fun Application.configureKoin(test: Boolean) {
-    install(Koin) {
-        if (!test) modules(prodModule)
-        else modules(testModule)
+    try {
+        install(Koin) {
+            if (!test) modules(prodModule)
+            else modules(testModule)
+        }
+    } catch (e: KoinAppAlreadyStartedException) {
+        log.warn("There is already a Koin application running! Skipping initialization...")
     }
 }
